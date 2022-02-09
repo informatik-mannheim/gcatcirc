bf6c71dbfaae7888f5ab48a597b4c83c9f020f2d
StarmanMartin <m.starman@live.com>
2022-02-09 10:45:22 +0100

    rust lib to standalone

--- src/rust_gcatcirc_lib/tests/lib_tests.rs
+++
@@ -1,9 +0,0 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        use rust_gcatcirc_lib::say_some;
-        println!("{}", u32::MAX);
-        assert_eq!(say_some(), "no id (['A'])) { A }");
-    }
-}
