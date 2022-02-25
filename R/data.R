c023531e4167845941e9dbe8eb9df0a97fdb617e
StarmanMartin <m.starman@live.com>
2022-02-25 08:07:22 +0100

    fix

--- R/data.R
+++
@@ -1,9 +0,0 @@
-#' List of all 216 maximal self complementary circular C3 codes.
-#'
-#' @docType data
-#'
-#' @format A list with 216 entries:
-#' \describe{
-#'   \item{code}{A maximal self complementary circular code}
-#' }
-"all_c3_codes"
